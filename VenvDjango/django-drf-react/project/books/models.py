8277afce288df3e43af96208bf4e09bc2fa25017
Will Greenberg <hello@will-greenberg.com>
2019-12-09 18:53:58 -0500

    add better views, add book fixtures to populate DB

--- VenvDjango/django-drf-react/project/books/models.py
+++ VenvDjango/django-drf-react/project/books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 # Create your models here.
 class Book(models.Model):
@@ -26,3 +27,12 @@ class Has(models.Model):
     condition = models.CharField(max_length=2, choices=CONDITION_CHOICES, default=GOOD)
     added_at = models.DateTimeField(auto_now_add=True)
 
+class ListingForm(ModelForm):
+    class Meta:
+        model = Has
+        fields = ("isbn", "condition",)
+
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = ("isbn", "title", "author", "edition",)
--- VenvDjango/django-drf-react/project/books/views.py
+++ VenvDjango/django-drf-react/project/books/views.py
@@ -2,8 +2,13 @@
 from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from books.models import Book, Has, User
+from books.models import Book, Has, User, ListingForm, BookForm
 from books.serializers import BookSerializer, HasSerializer, UserSerializer
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.decorators import login_required
+import datetime
 
 class BookListCreate(generics.ListCreateAPIView):
     queryset = Book.objects.all()
@@ -20,6 +25,61 @@ class UserList(generics.ListAPIView):
 class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+def home(request):
+    '''List of my current listings'''
+    if request.method == 'GET':
+        try:
+            listings = Has.objects.filter(user=request.user)
+        except TypeError:
+            listings = Has.objects.all()
+    return HttpResponse(listings, content_type='application/json')
+
+@login_required
+def listing(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        new_listing = form.save(commit=False)
+        new_listing.isbn = request.isbn
+        new_listing.user = request.user
+        new_listing.added_at = datetime.datetime.now()
+        new_listing.save()
+        return home(request)
+    else:
+        form = ListingForm()
+    return HttpResponse(form)
+
+def addBook(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        new_book = form.save(commit=False)
+        new_book.isbn = request.isbn
+        new_book.title = request.title
+        new_book.author = request.author
+        new_book.edition = request.edition
+        return home(request)
+    else:
+        form = BookForm()
+    return HttpResponse(form)
+
+def registerUser(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        new_user = form.save(commit=True)
+        user = authenticate(username=new_user.username,
+                            password=form.clean_password2())
+        if user is not None:
+            login(request, user)
+        else:
+            raise Exception
+        return home(request)
+    else:
+        form = UserCreationForm()
+    return HttpResponse(form)
+
+def login(request):
+    if request.method == 'POST':
+        form = User
 
 # Custom user registration view from https://stackoverflow.com/questions/16857450/how-to-register-users-in-django-rest-framework#29391122
 # @api_view(['POST'])
